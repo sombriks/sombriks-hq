a0da233e37f9c88be12bcfdac651fa137d17e603
sombriks <sombriks@gmail.com>
2017-10-18 15:59:37 -0300

    just fooling around

--- src/components/boneco.js
+++ src/components/boneco.js
@@ -2,7 +2,8 @@
 const PIXI = require("pixi.js");
 
 class Boneco {
-  constructor({ stage, atlas, position }) {
+
+  constructor({ app, stage, atlas, position }) {
     const loader = new PIXI.loaders.Loader();
     const key = "atlas" + new Date().getTime()
     loader.add(key, atlas).load((loader, resources) => {
@@ -15,7 +16,45 @@ class Boneco {
       this.sprite = new PIXI.extras.AnimatedSprite(anim);
       this.sprite.position.set(position.x, position.y);
       stage.addChild(this.sprite)
+      app.ticker.add(_ => {
+        if (this.walking)
+          this.walk()
+      })
     });
+
+  }
+
+  walk() {
+    const p = { x: this.sprite.x, y: this.sprite.y }
+    // console.log({ x: pos.x - p.x, y: pos.y - p.y })
+    const pos = this.pos
+    const near = this.near
+    const speed = this.speed
+    const dx = pos.x - p.x
+    const dy = pos.y - p.y
+    let stopx = false
+    let stopy = false
+    if (dx < -near)
+      this.sprite.x -= speed
+    else if (dx > near)
+      this.sprite.x += speed
+    else
+      stopx = true
+    if (dy < -near)
+      this.sprite.y -= speed
+    else if (dy >= near)
+      this.sprite.y += speed
+    else
+      stopy = true
+    if (stopx && stopy)
+      this.walking = false;
+  }
+
+  walkTo(pos, speed = 5, near = 10) {
+    this.pos = pos
+    this.near = near
+    this.speed = speed
+    this.walking = true
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ require("./index.css");
 const PIXI = require("pixi.js");
 const Letreiro = require("./components/letreiro");
 const Boneco = require("./components/boneco");
+const Camera = require("./components/camera");
 
 const app = new PIXI.Application(window.innerWidth, window.innerHeight);
 
 window.addEventListener("resize", _ => app.renderer.resize(window.innerWidth, window.innerHeight))
 document.getElementById("root").appendChild(app.view);
 
-new Boneco({ stage: app.stage, atlas: "assets/boneco.json", position: { x: window.innerWidth / 2, y: window.innerHeight * 0.7 } })
-new Letreiro({ stage: app.stage, texto: "em construção", position: { x: window.innerWidth / 2, y: window.innerHeight / 2 } })
+const stage = new Camera({ app })
+const player = new Boneco({ app, stage, atlas: "assets/boneco.json", position: { x: window.innerWidth / 2, y: window.innerHeight * 0.7 } })
+stage.follow(player)
+
+new Letreiro({ stage, texto: "em construção", position: { x: window.innerWidth / 2, y: window.innerHeight / 2 } })
+
