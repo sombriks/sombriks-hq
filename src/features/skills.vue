f9d5df832a4a9f0a7a55052c98393bf246311c0f
sombriks <sombriks@gmail.com>
2017-12-04 13:52:28 -0300

    my skills

--- src/features/skills.vue
+++ src/features/skills.vue
@@ -1,27 +1,21 @@
 <template>
   <div>
     <h1>Skills</h1>
-    <p>Boy! This is the funny part when i have to brag about myself a bit. Well, here we go.</p>
-    <p>I am a coder. This is what i do to live. I think about a problem, about a certain need. Then i code a solution.</p>
-    <p>And i can do that in several languages. I can do that in javasript. In Java. In SQL. In Python, in PHP, in Ruby.</p>
-    <p>To be honest, give me a week and i'll masterize anything the project might come to need.</p>
-    <h2>About Java and Javascript</h2>
-    <p>I do love that little devil. Back in 2007, a <a href="https://www.facebook.com/raphaelmonticello">mad dude</a> gave me a chance to do some serious work.</p>
-    <p>It was august. i got promoted from trainee to junior programmer in january.</p>
-    <p>And boy, we did some serious Java that time. The client had platform limitations, yet he wanted the best experience for their customers and brokers.</p>
-    <p>Since we where limited on server side (that was the java 6 eve but our solution had to comply to java 1.4 experience), all the magic had to be made on client</p>
-    <p>And there where, of course, internet explorer 6 standing on our way, making everything dificult and, more serious, slow.</p>
-    <p>At time, part of our solution was to avoid page reloading, putting page fragments inside a div and changing it basded on ulr hash value.</p>
-    <p>Years later i discovered that civilized people called it SPA - Single Page Application.</p>
-    <p>On the server part we had no fancy JPA or JSF 2 to play with. the quite old Webspehre 6 didn't handle well these new things.</p>
-    <p>iBatis (now known as MyBatis) was part of many solutions, it was my very frist contact with datamappers and, most important, the first time i heard about migrations.</p>
-    <p>These times showed me what came to be the most reasonable approach years after regarding of how to build huge projects.</p>
-    <p>Sometimes you have to comply client issues/limitations</p>
-    <p>Sometimes you have to drift responsibilites from server to client side. The <i>nowadays soilution</i> can confirm that.</p>
-    <h2>About Python, PHP, Ruby, ETC</h2>
-    <p>I had little contact with those languages, however i discovered that there's need one week or less to get versed with the basics.</p>
-    <p>I've toyed in the past with PHP. In the very past. Nowadays we have classes and laravel. That says a lot.</p> 
-    <p>On the python realm we have django and sqlalchemy.</p> 
+    <h2>Programming languages</h2>
+    <p>Javascript, Java, SQL, CSS, HTML, C, C++, PHP, Python, Ruby</p>
+    <h2>Platforms and runtimes</h2>
+    <p>Linux, Windows, JVM, Nodejs, MySQL, Postgresql</p>
+    <h2>Frameworks</h2>
+    <p>Express, Knex.js, Vue.js, Vue Material, mocha, chai, Bootstrap, Font Awesome, Material Design Icons, 
+      Angularjs (1.x), Angular material (1.x), JEE (JPA, JSF, JMS, etc.), Flywaydb, JUnit</p>
+    <h2>Cool tools and services</h2>
+    <p>Cordova, NPM, Browserify, PgAdmin3, iReport, Elastic Email, Onesignal, 
+      Make, CMake, Maven, Gradle, Ant</p>
+    <h2>Good pratices</h2>
+    <p>Funcional Programming, Object-oriented programming, Design Patterns (GOF: factory, facade, adapter, strategy, decorator, etc), 
+      Test-driven development, Unified modeling language, Model-view-controller, Model-view-view-model, Database Migrations</p>
+    <h2>Misc</h2>
+    <p>Project management, Requirements analysis, Scrum</p>
   </div>
 </template>
 
