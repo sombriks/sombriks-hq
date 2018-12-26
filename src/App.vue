2dc1ce0bab6e9404b1f3a97d8e3c881a2bde3fbc
sombriks <sombriks@gmail.com>
2018-12-26 13:21:59 -0300

    fix footer

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer app v-model="drawer" width="480">
-      <side-menu/>
-    </v-navigation-drawer>
-    <v-toolbar app>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <h1>{{title}}</h1>
-    </v-toolbar>
-    <v-content>
-      <v-container fluid>
-        <router-view></router-view>
-      </v-container>
-    </v-content>
-    <v-footer app></v-footer>
-  </v-app>
+	<v-app dark>
+		<v-navigation-drawer app v-model="drawer" width="480">
+			<side-menu/>
+		</v-navigation-drawer>
+		<v-toolbar app>
+			<v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+			<h1>{{title}}</h1>
+		</v-toolbar>
+		<v-content>
+			<v-container fluid>
+				<router-view></router-view>
+			</v-container>
+		</v-content>
+		<v-footer app>
+			<v-layout row align-center justify-center>&copy; sombriks (a.k.a. Leonardo Silveira) {{year}}</v-layout>
+		</v-footer>
+	</v-app>
 </template>
 
 <script>
 const { mapState } = require("vuex");
 module.exports = {
-  name: "App",
-  data: _ => ({ drawer: false }),
-  computed: mapState(["title"])
+	name: "App",
+	data: _ => ({ drawer: false, year: new Date().getFullYear() }),
+	computed: mapState(["title"])
 };
 </script>
