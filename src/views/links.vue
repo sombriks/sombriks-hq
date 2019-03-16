f7f788e3ca1f0716109e7febfe52f1c579c17c45
sombriks <sombriks@gmail.com>
2019-03-16 17:17:10 -0300

    new links

--- src/views/links.vue
+++ src/views/links.vue
@@ -38,6 +38,16 @@
 		<a target="_blank" href="https://www.timsommer.be/famous-laws-of-software-development/">Famous laws of software development</a>
 		<a target="_blank" href="http://raganwald.com/2019/02/14/i-love-programming-and-programmers.html">I like the way compilers work</a>
 		<a target="_blank" href="https://yuml.me/diagram/scruffy/class/draw">yUML does cool uml</a>
+		<a target="_blank" href="https://colorhunt.co">Nice and shiny colors</a>
+		<a target="_blank" href></a>
+		<a target="_blank" href></a>
+		<a target="_blank" href></a>
+		<a target="_blank" href></a>
+		<a target="_blank" href></a>
+		<a target="_blank" href></a>
+		<a target="_blank" href></a>
+		<a target="_blank" href></a>
+		<a target="_blank" href></a>
 		<a target="_blank" href></a>
 
 		<v-layout column align-center justify-center>
