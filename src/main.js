1b5256d6d3497b7c53cd6500242a959c3eb86876
sombriks <sombriks@gmail.com>
2022-03-29 17:37:15 -0300

    update tag manager

--- src/main.js
+++ src/main.js
@@ -3,13 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import VueAnalytics from "vue-analytics";
-
-Vue.use(VueAnalytics, {
-  id: "UA-37990292-20",
-  router,
-});
-
 Vue.config.productionTip = false;
 
 const name = "sombriks-hq";
