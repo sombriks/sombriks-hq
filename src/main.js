46e80d66cb0a441570020d72ca2794fe9ed3944b
sombriks <sombriks@gmail.com>
2022-04-06 16:42:11 -0300

    restoring google analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from "vue";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+import VueGtag from "vue-gtag";
+4;
+Vue.use(
+  VueGtag,
+  {
+    config: { id: "G-SRC55ZDV0D" },
+  },
+  router
+);
 
 Vue.config.productionTip = false;
 
