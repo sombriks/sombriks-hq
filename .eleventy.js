2965908a78e42ce35605aacf2d72ae5f079f4324
sombriks <sombriks@gmail.com>
2023-02-14 02:28:42 -0300

    montando publicação na aws

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
