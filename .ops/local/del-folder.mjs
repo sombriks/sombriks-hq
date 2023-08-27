6e093309d64fedd60a26cdeb88dd92bd2dd14341
Leonardo Silveira <sombriks@gmail.com>
2023-08-27 15:45:38 -0300

    updating dependencies and remove housemade ops tooling

--- .ops/local/del-folder.mjs
+++
@@ -1,8 +0,0 @@
-import fs from "fs"
-
-export const del = (path) => {
-    console.log(`deleting ${path}`)
-    fs.rmSync(path, { recursive: true, force: true });
-    console.log("done")
-}
-
--- .ops/tasks/clean-local.mjs
+++
@@ -1,6 +0,0 @@
-import {del} from "../local/del-folder.mjs";
-import {checkEnvironment} from "../local/basic-checks.mjs";
-
-checkEnvironment()
-
-del(process.env.HQ_DIST_SITE_FOLDER)
