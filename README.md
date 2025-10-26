bc5b54b9b3f0d82ec4d63f9f700b79952ae5f55c
Leonardo Silveira <sombriks@gmail.com>
2025-10-26 17:47:48 -0300

    nova estrutura

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# sombriks-hq
+# [sombriks-hq][sombriks-hq]
 
-My personal blog
+My personal blog.
+
+made with [eleventy][eleventy], [pug][pug], 
+[the-monospace-web][the-monospace-web] and _some love_.
+
+[sombriks-hq]: https://github.com/sombriks/sombriks-hq
+[eleventy]: https://www.11ty.dev/
+[pug]: https://devdocs.io/pug/
+[the-monospace-web]: https://owickstrom.github.io/the-monospace-web/
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,14 @@
 import pugPlugin from "@11ty/eleventy-plugin-pug";
 
 export default function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("public");
+  // static assets
+  eleventyConfig.addPassthroughCopy({"node_modules/@owickstrom/the-monospace-web/src": "/the-monospace-web"});
+  eleventyConfig.addPassthroughCopy({"public": "/"});
+  // root folder and others
   eleventyConfig.setInputDirectory("src");
+  eleventyConfig.setLayoutsDirectory("layouts");
+  // destination
   eleventyConfig.setOutputDirectory("dist");
+  // misc plugins
   eleventyConfig.addPlugin(pugPlugin);
 }
