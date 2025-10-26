17638c5db3124bf1c469a1274912e3a66bbec6c7
Leonardo Silveira <sombriks@gmail.com>
2025-10-26 14:37:34 -0300

    nova estrutura

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,6 @@ import pugPlugin from "@11ty/eleventy-plugin-pug";
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("public");
   eleventyConfig.setInputDirectory("src");
-  eleventyConfig.addPlugin(pugPlugin);
   eleventyConfig.setOutputDirectory("dist");
+  eleventyConfig.addPlugin(pugPlugin);
 }
