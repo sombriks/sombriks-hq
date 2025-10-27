fbef245e109c957bd6c462410b4fa041ed28b2f8
Leonardo Silveira <sombriks@gmail.com>
2025-10-26 23:53:52 -0300

    estruturando novo blog

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,18 +1,21 @@
 import pugPlugin from "@11ty/eleventy-plugin-pug";
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+import YAML from "yaml";
 
 export default function (eleventyConfig) {
   // static assets
-  eleventyConfig.addPassthroughCopy({"node_modules/@owickstrom/the-monospace-web/src": "/the-monospace-web"});
-  eleventyConfig.addPassthroughCopy({"public": "/"});
+  eleventyConfig.addPassthroughCopy({ "node_modules/@owickstrom/the-monospace-web/src": "/the-monospace-web" });
+  eleventyConfig.addPassthroughCopy({ "public": "/" });
   // root folder and others
   eleventyConfig.setInputDirectory("src");
   eleventyConfig.setIncludesDirectory("components")
   eleventyConfig.setLayoutsDirectory("layouts");
+  eleventyConfig.setDataDirectory("data");
   // destination
   eleventyConfig.setOutputDirectory("dist");
   // misc plugins
   eleventyConfig.addPlugin(pugPlugin);
+  eleventyConfig.addDataExtension("yml,yaml", (contents) => YAML.parse(contents));
   eleventyConfig.addPlugin(feedPlugin, {
     type: "rss", // or "rss", "json"
     outputPath: "/feed.xml",
