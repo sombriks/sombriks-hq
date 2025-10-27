822688e2eda76c213a378d5371a3c9f3da4986a3
Leonardo Silveira <sombriks@gmail.com>
2025-10-26 21:57:40 -0300

    estruturando

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 import pugPlugin from "@11ty/eleventy-plugin-pug";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 
 export default function (eleventyConfig) {
   // static assets
@@ -12,4 +13,22 @@ export default function (eleventyConfig) {
   eleventyConfig.setOutputDirectory("dist");
   // misc plugins
   eleventyConfig.addPlugin(pugPlugin);
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: "rss", // or "rss", "json"
+    outputPath: "/feed.xml",
+    collection: {
+      name: "posts", // iterate over `collections.posts`
+      limit: 10,     // 0 means no limit
+    },
+    metadata: {
+      language: "en",
+      title: "Sombriks Has a Plan",
+      subtitle: "Technology, opensource and so on",
+      base: "https://sombriks.com/",
+      author: {
+        name: "Leonardo Silveira",
+        email: "", // Optional
+      }
+    }
+  });
 }
