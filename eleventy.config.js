65ada0f1f6b7ab20f7917512f3d9d9da7f7af40f
Leonardo Silveira <sombriks@gmail.com>
2025-11-07 18:32:46 -0300

    tma css do destaque de código selecionado

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,9 +3,13 @@ import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import YAML from "yaml";
 
+/**
+ * @param {import("@11ty/eleventy")} eleventyConfig
+ */
 export default function (eleventyConfig) {
   // static assets
   eleventyConfig.addPassthroughCopy({ "node_modules/@owickstrom/the-monospace-web/src": "/the-monospace-web" });
+  eleventyConfig.addPassthroughCopy({ "node_modules/prism-themes/themes": "/prism-themes" });
   eleventyConfig.addPassthroughCopy({ "node_modules/prismjs/themes": "/prismjs" });
   eleventyConfig.addPassthroughCopy({ "public": "/" });
   // root folder and others
